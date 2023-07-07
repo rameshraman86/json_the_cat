7559c79816ff7e872f776e11c8c734431797ed1c
Ramesh Raman <2029381+rameshraman86@users.noreply.github.com>
2023-07-07 15:57:34 -0400

    cat as service

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,18 @@
 const request = require('request');
-const input = process.argv;
-const breedName = input.slice(2, 3);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  }
-  else {
-    const data = JSON.parse(body);
-    if (data.length === 0) {
-      console.log(`Breed not found`);
+const fetchBreedDescription = (breedName, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
     } else {
-      console.log(data[0].description);
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        callback('breed not found', null);
+      } else {
+        callback(null, data[0].description);
+      }
     }
-  }
-});
+  });
+};
+
+module.exports = { fetchBreedDescription };
